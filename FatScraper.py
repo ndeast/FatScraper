be24b7d20e67e923a20d3661690967f455e83367
Nik Eastburn <nik@ndeast.com>
2018-08-19 18:56:00 -0400

    check for dupes before adding to db

--- FatScraper.py
+++ FatScraper.py
@@ -1,30 +1,40 @@
 from UpcomingRecord import Base, UpcomingRecord, UpcomingRecordSchema
 import FatWreckScraper
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, exists
 from sqlalchemy.orm import sessionmaker
+
+
+newRelease = False
 
 # Connect to DB and create session
 engine = create_engine('sqlite:///db/UpcomingRecords.db')
 Base.metadata.bind = engine
 
+schema = UpcomingRecordSchema(many=True)
+
 DBSession = sessionmaker()
 DBSession.bind = engine
 session = DBSession()
 ###################################
-
-schema = UpcomingRecordSchema(many=True)
-
+#
+# Add any new additions to database
+current_rec_titles = [rec.title for rec in session.query(UpcomingRecord.title)]
 upRecs = FatWreckScraper.scrape()
 
 for rec in upRecs:
-    session.add(rec)
-session.commit()
+    if rec.title not in current_rec_titles:
+        session.add(rec)
+        session.commit()
+        newRelease = True
 
-recDB = session.query(UpcomingRecord).all()
+# output new releases to json file
+if newRelease:
+    updated_recDB = session.query(UpcomingRecord).all()
 
-jsonOutput = schema.dumps(recDB)
-with open("UpcomingRecords.txt", 'w') as outFile:
-    outFile.write(jsonOutput)
+    jsonOutput = schema.dumps(updated_recDB)
+    with open("UpcomingRecords.txt", 'w') as outFile:
+        outFile.write(jsonOutput)
+
 
 session.close()
 engine.dispose()
