20cb9e7637f20880b3b8668bf6421b71c767ce24
Nik Eastburn <nik@ndeast.com>
2019-09-11 21:51:26 -0400

    Check if upRecs is empty instead of NoneType

--- FatScraper.py
+++ FatScraper.py
@@ -3,7 +3,6 @@ import FatWreckScraper
 from sqlalchemy import create_engine, exists
 from sqlalchemy.orm import sessionmaker
 
-
 newRelease = False
 # Connect to DB and create session
 engine = create_engine('sqlite:///db/UpcomingRecords.db')
@@ -14,15 +13,15 @@ schema = UpcomingRecordSchema(many=True)
 DBSession = sessionmaker()
 DBSession.bind = engine
 session = DBSession()
+
 ###################################
-#
 # Pull existing titles and links
 current_rec_titles = [rec.title for rec in session.query(UpcomingRecord.title)]
 current_rec_links = [rec.link for rec in session.query(UpcomingRecord.link)]
 upRecs = FatWreckScraper.scrape(current_rec_links)
 ###################################
-# 
-if upRecs is not None:
+
+if upRecs:
     for rec in upRecs:
         if rec.title not in current_rec_titles:
             session.add(rec)
@@ -33,7 +32,6 @@ if upRecs is not None:
 # output new releases to json file
 if newRelease:
     updated_recDB = session.query(UpcomingRecord).all()
-
     jsonOutput = schema.dumps(updated_recDB)
     with open("UpcomingRecords.json", 'w') as outFile:
         outFile.write(jsonOutput)
